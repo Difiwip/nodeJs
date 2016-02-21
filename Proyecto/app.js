0bd768fb5dade064120f49b23ffa02d1a1e94811
Nico <nico97953@gmail.com>
2016-02-21 00:40:56 -0300

    Añadido sistema de logeo y registro

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -1,4 +1,4 @@
-var http = require("http"); // Variable http
+
 var express = require("express");
 var mongoose = require("mongoose");
 var bodyParse = require("body-parser");
@@ -7,13 +7,15 @@ var Schema = mongoose.Schema;
 mongoose.connect("mongodb://localhost/fotos");
 
 var userSchemaJSON = {
+	nick: String,
 	email: String,
 	password: String,
 };
 
+
 var user_schema = new Schema(userSchemaJSON);
 
-var User = mongoose.model("User",user_schema);
+var User = mongoose.model("Usuarios",user_schema);
 
 var app = express();
 
@@ -26,18 +28,30 @@ app.get("/",function(req,res) {
 	res.render("index");
 });
 
-app.get("/peticion",function(req,res){
-	User.find(function(err,doc){
-		console.log(doc);
-	});
+app.get("/login",function(req,res){
 	res.render("peticion");
 });
 
-app.post("/confirmado",function(req,res){
-	var user = new User({email: req.body.mail, password: req.body.password});
+app.get("/register",function(req,res){
+	res.render("register");
+})
+
+app.post("/login",function(req,res){
+	var user = new User({nick: req.body.nick, email: req.body.mail, password: req.body.password});
 
 	user.save(function(){
-		res.send("Usuario almacenado");
+		res.render("peticion");
+	});
+});
+
+app.post("/welcome",function(req,res){
+	User.findOne({nick: req.body.nick, password: req.body.password},function(err,doc){
+		if (doc) {
+			res.send("esta bien");
+		}
+		else {
+			res.send("esta mal");
+		}
 	});
 });
 
