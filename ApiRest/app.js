1849cf8aeb9117f99277bba7f128d9d88f2dd060
Nico <nico97953@gmail.com>
2016-03-03 11:44:01 -0300

    Comentado

--- ApiRest/app.js
+++ ApiRest/app.js
@@ -1,6 +1,5 @@
 var express = require("express"),
     bodyParser = require("body-parser"),
-    methodOverride = require("method-override");
     User = require("./models/user").User;
 
 var app = express();
@@ -9,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-var router = express.Router();
+var router = express.Router(); // Declaro la ruta
 
 router.use(function(req,res,next){
   console.log("Request a la api");
@@ -17,56 +16,55 @@ router.use(function(req,res,next){
 });
 
 
-router.get("/",function(req,res){
-  res.send("hola");
-});
+router.route("/users") // En la ruta /Users abajo declaro los verbos y lo que va a pasar
 
-router.route("/users")
-  .post(function(req,res){
+  .post(function(req,res){ // En el verbo http POST tengo que crear un nuevo users
+
     var user = new User();
         user.nick = req.body.nick;
         user.email = req.body.email;
-        user.password = req.body.password;
+        user.password = req.body.password; // hago los parseos de todo
 
-        user.save(function(err){
+        user.save(function(err){ // Lo guardo
           if (err) {
-            res.send("Error al crear usuario: " + err);
+            res.send("Error al crear usuario: " + err); // Si hay error me putea
           }
           res.send("Usuario creado con exito");
         });
   })
-  .get(function(req,res){
-    User.find(function(err,doc){
-      if(err) res.send(err);
-      res.json(doc);
+
+  .get(function(req,res){ // Verbo get me muestra todos los users guardados
+    User.find(function(err,doc){ // El documento se guarda en el segundo parametro doc
+      if(err) res.send(err); // Si hay error me putea
+      res.json(doc);// Lo muestro como json
     });
   });
 
-router.route("/users/:id")
+router.route("/users/:id") // Ahora a la ruta /users pero con una id especifica
 
-  .get(function(req,res){
-    User.findById(req.params.id,function(err,doc){
+  .get(function(req,res){ // Igual que el get anterior solo que ahora en findById le paso la id del user que quiero mostrar
+    User.findById(req.params.id,function(err,user){ // Lo guardo en user
       if(err) console.log(err);;
-      res.json(doc);
+      res.json(user); // lo muestro
       })
     })
 
     .put(function(req,res){
-      User.findById(req.params.id,function(err,user){
+
+      User.findById(req.params.id,function(err,user){ // Encuentro el user con el id, y lo vuelvo a guardar en el segundo parametro "user"
 
             user.nick = req.body.nick;
             user.email = req.body.email;
-            user.password = req.body.password;
+            user.password = req.body.password; // Una vez localizado actualizo los datos
 
             user.save(function(err){
               if (err) {
-                res.send("Error al actualizar el  usuario: " + err);
+                res.send("Error al actualizar el  usuario: " + err); // Error
               }
-              res.send("Usuario actualizado con exito");
+              res.send("Usuario actualizado con exito"); // tanan!!
             });
       });
     });
-
 
 
 app.use(router);
